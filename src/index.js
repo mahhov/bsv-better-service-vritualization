6873e0a2b017a43b7f74b4cedd9a661b1007692a
m <mahhov1@gmail.com>
2018-03-04 17:49:23 -0800

    allow mode to be set async and remove function wrapping

--- src/index.js
+++ src/index.js
@@ -1,156 +1,151 @@
-(function () {
-    let bsv = {};
+let bsv = {};
 
-    let modes = {'IGNORE': 0, 'RECORD': 1, 'REPLAY': 2};
+let modes = {'IGNORE': 0, 'RECORD': 1, 'REPLAY': 2};
+let modeResolve;
+let modePromise = new Promise(resolve => {
+    modeResolve = resolve
+});
+let replayDelay;
+let customReplayDelays = {};
+let recordings = {};
+let replayHistory = {};
 
-    let mode = modes.IGNORE;
-    let replayDelay;
-    let customReplayDelays = {};
-    let recordings = {};
-    let replayHistory = {};
+bsv.setModeIgnore = () => {
+    modeResolve(modes.IGNORE);
+};
 
-    bsv.setModeIgnore = () => {
-        mode = modes.IGNORE;
-    };
+bsv.setModeRecord = () => {
+    modeResolve(modes.RECORD);
+};
 
-    bsv.setModeRecord = () => {
-        mode = modes.RECORD;
-    };
+bsv.setModeReplay = () => {
+    modeResolve(modes.REPLAY);
+};
 
-    bsv.setModeReplay = () => {
-        mode = modes.REPLAY;
-    };
+bsv.setReplayDelay = delay => {
+    replayDelay = delay;
+};
 
-    bsv.setReplayDelay = delay => {
-        replayDelay = delay;
-    };
+bsv.setCustomReplayDelay = (name, delay) => {
+    customReplayDelays[name] = delay;
+};
 
-    bsv.setCustomReplayDelay = (name, delay) => {
-        customReplayDelays[name] = delay;
-    };
+bsv.export = bsv.exportClipboard = () => {
+    copy(recordings);
+};
 
-    bsv.export = bsv.exportClipboard = () => {
-        copy(recordings);
-    };
+bsv.exportObject = () => recordings;
 
-    bsv.exportObject = () => recordings;
+bsv.exportString = () => JSON.stringify(recordings);
 
-    bsv.exportString = () => JSON.stringify(recordings);
+bsv.exportFile = fileName => {
+    let dataString = `data:text/json,${JSON.stringify(recordings)}`;
+    let elem = document.createElement('a');
+    elem.setAttribute('href', dataString);
+    elem.setAttribute('download', fileName);
+    elem.click();
+    elem.remove();
+};
 
-    bsv.exportFile = fileName => {
-        let dataString = `data:text/json,${JSON.stringify(recordings)}`;
-        let elem = document.createElement('a');
-        elem.setAttribute('href', dataString);
-        elem.setAttribute('download', fileName);
-        elem.click();
-        elem.remove();
-    };
+bsv.import = recordingsJson => {
+    recordings = recordingsJson;
+};
 
-    bsv.import = recordingsJson => {
-        recordings = recordingsJson;
-    };
+bsv.registerPromise = (name, object, method) => {
+    let oldMethod = object[method];
 
-    bsv.registerPromise = (name, object, method) => {
-        if (mode === modes.RECORD)
-            recordPromise(name, object, method);
-        else if (mode === modes.REPLAY)
-            replayPromise(name, object, method);
-    };
+    object[method] = function () {
+        return modePromise.then(mode => {
+            if (mode === modes.RECORD) {
+                if (!recordings[name])
+                    recordings[name] = [];
 
-    let recordPromise = (name, object, method) => {
-        if (!recordings[name])
-            recordings[name] = [];
+                let response = oldMethod.call(this, ...arguments);
+                let recordArguments = _.map(arguments, argument => argument);
+                response.then(resolution => {
+                    recordings[name].push({'arguments': recordArguments, 'resolution': resolution, 'resolved': true});
+                }).catch(rejection => {
+                    recordings[name].push({'arguments': recordArguments, 'rejection': rejection});
+                });
+                return response;
 
-        let oldMethod = object[method];
+            } else if (mode === modes.REPLAY) {
+                if (!recordings || !recordings[name] || !recordings[name].length) {
+                    warning404(name);
+                    return new Promise();
+                }
 
-        object[method] = function () {
-            let response = oldMethod.call(this, ...arguments);
-            let recordArguments = _.map(arguments, argument => argument);
-            response.then(resolution => {
-                recordings[name].push({'arguments': recordArguments, 'resolution': resolution, 'resolved': true});
-            }).catch(rejection => {
-                recordings[name].push({'arguments': recordArguments, 'rejection': rejection});
-            });
-            return response;
-        };
-    };
+                if (!replayHistory[name])
+                    replayHistory[name] = 0;
 
-    let replayPromise = (name, object, method) => {
-        if (!object) {
-            warningNull(name);
-            return;
-        }
+                let index = replayHistory[name];
+                replayHistory[name] < recordings[name].length - 1 && replayHistory[name]++;
+                let recording = recordings[name][index];
+                let delay = customReplayDelays[name] || replayDelay;
+                console.log('invoking', name, 'with', delay, 'delay');
 
-        if (!replayHistory[name])
-            replayHistory[name] = 0;
-
-        object[method] = () => {
-            if (!recordings || !recordings[name]) {
-                warning404(name);
-                return Promise.reject();
-            }
-
-            let index = replayHistory[name];
-            replayHistory[name] < recordings[name].length - 1 && replayHistory[name]++;
-            let recording = recordings[name][index];
-            if (!recording) {
-                warning404(name, index);
-                return Promise.reject();
-            }
-
-            let delay = customReplayDelays[name] || replayDelay;
-            console.log('invoking', name, 'with', delay, 'delay');
-
-            if (!delay)
-                return recording.resolved ? Promise.resolve(recording.resolution) : Promise.reject(recording.rejection);
-            else
-                return recording.resolved ?
-                    new Promise(resolve => {
-                        setTimeout(resolve, delay, recording.resolution);
-                    }) :
-                    new Promise((resolve, reject) => {
-                        setTimeout(reject, delay, recording.rejection);
+                if (!delay)
+                    return recording.resolved ? recording.resolution : Promise.reject(recording.rejection);
+                else
+                    return new Promise((resolve, reject) => {
+                        setTimeout(() => {
+                            recording.resolved ? resolve(recording.resolution) : reject(recording.rejection);
+                        }, delay);
                     });
-        };
-    };
 
-    bsv.registerField = (name, object, field) => {
+            } else
+                return oldMethod.call(this, ...arguments);
+        });
+    };
+};
+
+let recordPromise = (name, object, field, oldMethod) => {
+
+};
+
+let replayPromise = (name, object, method, oldMethod) => {
+};
+
+// todo extract record and replay promise to external functions
+
+bsv.registerField = (name, object, field) => {
+    modePromise.then(mode => {
         if (mode === modes.RECORD)
             recordField(name, object, field);
         else if (mode === modes.REPLAY)
             replayField(name, object, field);
-    };
+    });
+};
 
-    let recordField = (name, object, field) => {
-        if (!recordings[name])
-            recordings[name] = {};
+let recordField = (name, object, field) => {
+    if (!recordings[name])
+        recordings[name] = {};
 
-        recordings[name][field] = object[field];
-    };
+    recordings[name][field] = object[field];
+};
 
-    let replayField = (name, object, field) => {
-        if (!object)
-            warningNull(name);
-        else if (!recordings || !recordings[name])
-            warning404(name);
-        else
-            object[field] = recordings[name][field];
-    };
+let replayField = (name, object, field) => {
+    if (!object)
+        warningNull(name);
+    else if (!recordings || !recordings[name])
+        warning404(name);
+    else
+        object[field] = recordings[name][field];
+};
 
-    let warningNull = name => {
-        warning('null object', name);
-    };
+let warningNull = name => {
+    warning('null object', name);
+};
 
-    let warning404 = (name, details) => {
-        warning('no recordings found for', name, details);
-    };
+let warning404 = (name, details) => {
+    warning('no recordings found for', name, details);
+};
 
-    let warning = (message, name, details) => {
-        console.error(message, name, details ? details : '');
-    };
+let warning = (message, name, details) => {
+    console.error(message, name, details ? details : '');
+};
 
-    window.bsv = bsv;
-})();
+window.bsv = bsv;
 
 // todo
 // replay based on arguments
